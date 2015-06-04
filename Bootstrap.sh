5c7c9ab87b32d429c41523fd80f0facfd8e878c2
Roger Caminal Armadans <caminal@pb-d-128-141-141-76.cern.ch>
2015-06-04 16:39:21 +0200

    New bootstrap version

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,14 +1,16 @@
 
 # Download the package
-git clone https://github.com/vimartin/H4bAnalysis.git
-cd H4bAnalysis
+#git clone https://github.com/vimartin/H4bAnalysis.git
+#cd H4bAnalysis
 
 # compile fastjet
 # note $(WORKDIR) is the current working area
-export WORKDIR=$PWD
+curl -O http://fastjet.fr/repo/fastjet-3.1.2.tar.gz
+tar -xvzf fastjet-3.1.2.tar.gz
 rm -rf fastjet/*
 cd fastjet-3.1.2
-./configure --prefix=$WORKDIR/fastjet/
+export WORKDIR=$PWD
+./configure --prefix=${WORKDIR}/fastjet/
 make install
 
 # Prepare and install the code
